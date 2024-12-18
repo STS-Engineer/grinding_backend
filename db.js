33b0c9a05c9cfb23009e372c34c8cdc2d023e3e6
STS-Engineer <administration.sts@avocarbon.com>
2024-12-18 18:04:20 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  user: 'adminavo',  // Matches the user in docker-compose
+  host: 'avo-adb-001.postgres.database.azure.com',  // 'db' is the service name in Docker Compose, not 'localhost'
+  database: 'web_app_competitor',  // The database name as per docker-compose
+  password: '$#fKcdXPg4@ue8AW',  // Password as per docker-compose
+  port: 5432,  // Default PostgreSQL port
+  ssl: { rejectUnauthorized: false },  // Enable SSL if you're connecting to Azure
+});
 
-const pool = new Pool ({
-    user: 'postgres',  
-    host: 'localhost',  
-    database: 'postgres',  
-    password: 'postgres',  
-    port: 5432,  
-    ssl: false,  
-})
-
-module.exports= pool;
+module.exports = pool;
