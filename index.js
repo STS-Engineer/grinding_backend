6b0a5f558158f33c57442c7b941014cd70efa1e3
STS-Engineer <administration.sts@avocarbon.com>
2024-12-23 15:23:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const prodformrouter = require('./services/formulaireprod');
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use(cors({
-    origin: 'https://grinding-backend.azurewebsites.net'
-  }));
+  origin: 'https://grinding-same.azurewebsites.net', // Allow frontend origin
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Explicitly allow these methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify headers allowed in requests
+  credentials: true, // Allow credentials (if using cookies or authentication headers)
+}));
   
 app.use('/ajouter', prodformrouter)
 
