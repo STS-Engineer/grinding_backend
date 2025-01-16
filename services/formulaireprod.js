75dd45a4c6dfc87b5303db0479ddbc6d290c47d6
STS-Engineer <administration.sts@avocarbon.com>
2025-01-16 10:33:15 +0100

    Update formulaireprod.js

--- services/formulaireprod.js
+++ services/formulaireprod.js
@@ -975,6 +975,24 @@ router.post('/ajouterprobleme', authenticate, async (req, res) => {
     });
   }
 });
+
+router.get('/getproblemes', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM problemetechnique');
+
+    // Assuming result.rows contains the data
+    return res.status(200).json({
+      message: 'Probleme retrieved successfully',
+      operateurs: result.rows, // Sending only the data rows
+    });
+  } catch (error) {
+    console.error('Failed to retrieve Probleme:', error);
+    return res.status(500).json({
+      message: 'Failed to retrieve Probleme',
+      error: error.message, // Optional: Include the error message for debugging
+    });
+  }
+});
 //defauts
 router.post('/ajouterdefaut', authenticate, async (req, res) => {
   const { defaut } = req.body;
